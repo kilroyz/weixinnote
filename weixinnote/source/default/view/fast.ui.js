0caa163a5586b42ebb3db5c2b035ff1acab73dc0
kilroyz <kilroyZ@163.com>
2015-09-30 22:02:09 +0800

    完成了除通讯录页面的所有页面

--- weixinnote/source/default/view/fast.ui.js
+++
@@ -1,6 +0,0 @@
-//related to fast.ui
-
-var bg_layout = ui("bground");
-bg_layout.on("touch",function(){
-	bg_layout.remove();
-});
--- weixinnote/source/default/view/indexlistview_cell/cell3.ui.js
+++
@@ -1,4 +0,0 @@
-
-//设置映射关系
-ui("$").setMapping({"number.text":"number"});
-//related to cell3.ui
